5ac76b12c7a63c27a189577cac2813d51c0e6e44
Fauzan Naufan <fauzannaufan@gmail.com>
2025-09-28 00:22:53 +0700

    feat: initial commit

---
+++ generator/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const fetch = require("node-fetch-commonjs");
+const {dirname} = require("path");
+
+let baseUrl = 'http://localhost/fr_afc/index.php';
+const filepath = '../dist';
+
+const replaceBaseUrl = (html) => {
+  return html.replaceAll(baseUrl, 'https://footyrankings.github.io');
+}
+
+const rootPath = ['', 'database', 'formula'];
+rootPath.forEach(async (path) => {
+  const response = await fetch(`${baseUrl}/${path}`);
+  const html = await response.text();
+  fs.mkdirSync(dirname(`${filepath}/${path}/index.html`), { recursive: true });
+  fs.writeFileSync(
+		`${filepath}/${path}/index.html`,
+		replaceBaseUrl(html)
+	);
+})
