2b97f2e5a92e2ff962a25c7f19cf9dbae73539c9
Fauzan Naufan <fauzannaufan@gmail.com>
2026-08-31 23:25:45 +0700

    ica53

--- docs/ica53/_next/static/Y3IBdZyt6ait09MIc4ywd/_buildManifest.js
+++ docs/ica53/_next/static/Y3IBdZyt6ait09MIc4ywd/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ica53//_next/:path+",
+        "destination": "/ica53/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
